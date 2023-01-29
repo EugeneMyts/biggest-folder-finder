efdde9a9cb19b91789e5935ec77701fb8b495338
Eugene <yevhenii.myts@gmail.com>
2023-01-29 04:41:21 -0500

    Foldersize calculator added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
 
 
+
         String folderPath = "C:\\Users\\Eugene\\Desktop\\testpapkiforproject";
 File file = new File(folderPath);
 
+long start =  System.currentTimeMillis();
 
-        System.out.println(getFolderSize(file));
+FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
 
+
+//        System.out.println(getFolderSize(file));
+long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + " ms");
 
 
     }
