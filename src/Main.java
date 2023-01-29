096bd77ed877554da57338e2521514c4e371f741
Eugene <yevhenii.myts@gmail.com>
2023-01-29 12:04:22 -0500

    Final process

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,43 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
+
 public class Main {
+
+
+
     public static void main(String[] args) {
 
+        ParametersBag bag = new ParametersBag(args);
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
 
 
-        String folderPath = "C:\\Users\\Eugene\\Desktop\\testpapkiforproject";
-File file = new File(folderPath);
 
-long start =  System.currentTimeMillis();
 
-FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+
+        File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
+
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
+       pool.invoke(calculator);
 
 
-//        System.out.println(getFolderSize(file));
-long duration = (System.currentTimeMillis() - start);
+        System.out.println(root);
+
+
+        long duration = (System.currentTimeMillis() - start);
         System.out.println(duration + " ms");
 
 
     }
 
 
-    public static long getFolderSize(File folder) {
 
-        if (folder.isFile()) {
-            return folder.length();
-        }
-
-
-        long sum = 0;
-        File [] files = folder.listFiles();
-        for (File file : files) {
-
-            sum += getFolderSize(file);
-
-        }
-
-
-
-
-
-return sum;
-
-    }
 
 
 }
---
+++ src/SizeCalculator.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class SizeCalculator {
+
+    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
+  private static  HashMap<Character, Integer> char2mulptiplier = getMultipliers();
+
+
+    public static String getHumanReadableSize(long size) {
+
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+
+
+            double value = ((double) size) / Math.pow(1024., i);
+
+            if (value < 1024) {
+                return Math.round(value * 100)/100. + " " + sizeMultipliers[i] +
+                        (i > 0 ? "b" : "");
+
+            }
+
+        }
+        return "Very big!";
+
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+
+
+        char sizeFactor = size
+                .replaceAll("[0-9\\s+]+", "")
+                .charAt(0);
+        int mulptiplier = char2mulptiplier.get(sizeFactor);
+        long length = mulptiplier * Long.valueOf(size.replaceAll("[^0-9]", ""));
+
+        return length;
+    }
+
+
+    private  static HashMap<Character, Integer> getMultipliers() {
+
+
+        HashMap<Character, Integer> char2mupltiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2mupltiplier.put(sizeMultipliers[i] ,(int) Math.pow(1024, i));
+        }
+
+        return char2mupltiplier;
+    }
+
+
+}
